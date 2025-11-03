1d2e4370d5ed3a1c740ba9806bdc19489821ddb7
yas033 <yas033@ucsd.edu>
2025-11-02 16:32:16 -0800

    Delete src directory

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { MenuScreenController } from "./MenuScreenController";
-
-// Mount the menu on the #container from index.html
-const root = (document.getElementById("container") as HTMLElement) || document.body;
-new MenuScreenController(root);
